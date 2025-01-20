00f5e491f804f7f303f30597ccedd22846a7946b
Naveen <naveen.ccmsd@gmail.com>
2025-01-20 15:59:15 +0530

    commands

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 set ELODIE_APPLICATION_DIRECTORY=.elodie
-python elodie.py import --debug  --move-source --destination="D:\images-organised" "D:\images-organised_old"
+python elodie.py import --debug  --move-source --destination="D:\images-organised" "C:\Users\naveen\Downloads\Photos-001"
+pause
